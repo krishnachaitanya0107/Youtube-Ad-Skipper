9f1edd4c6cf9a152c8889721b5c07b8a7571da75
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2023-04-22 22:10:22 +0530

    Cleaned up and optimized code

--- adskipper.py
+++ adskipper.py
@@ -2,80 +2,88 @@ import cv2
 import numpy as np
 import pyautogui
 import os
-import tkfontawesome
+import tkinter.font as tkfont
+import tkinter as tk
 
 
-from tkinter import *
-window = Tk()
-window.title('Youtube AD Skipper')
-window.geometry('500x500')
+class YoutubeAdSkipper:
+    def __init__(self, threshold=0.7):
+        self.threshold = threshold
+        self.templates = []
+        self.task = None
+        self.load_templates()
 
-cur_dir_path = os.path.abspath(os.path.dirname(__file__))
-template3 = cv2.imread(cur_dir_path+'\\template3.png', 0)
-template4 = cv2.imread(cur_dir_path+'\\template4.png', 0)
-template5 = cv2.imread(cur_dir_path+'\\template5.png', 0)
-template6 = cv2.imread(cur_dir_path+'\\template6.png', 0)
-threshold = 0.7
-task = []
+        self.window = tk.Tk()
+        self.window.title('Youtube AD Skipper')
+        self.window.geometry('500x500')
+        self.window.config(bg='black')
+
+        self.canvas = tk.Canvas(self.window, width=500, height=500)
+        self.canvas.pack(fill='both', expand=True)
+
+        self.play_icon = tk.PhotoImage(file='play.png')
+        self.stop_icon = tk.PhotoImage(file='stop.png')
+
+        self.window.iconphoto(True, self.play_icon)
+
+        self.window.bind("<Escape>", lambda e: self.window.quit())
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        font = tkfont.Font(family='Comic Sans MS', size=20, weight='bold')
+
+        img = tk.PhotoImage(file='YoutubeLogo.png')
+        self.canvas.create_image(140, 100, image=img, anchor="nw")
+
+        self.canvas.create_text(240, 50, text='Youtube Ad Skipper',
+                                font=font, fill='white')
+
+        play_btn = tk.Button(self.window,
+                             command=self.start, width=60, height=60, image=self.play_icon,
+                             bg='black', bd=0, activebackground='black')
+
+        stop_btn = tk.Button(self.window,
+                             command=self.stop, width=60, height=60, image=self.stop_icon,
+                             bg='black', bd=0, activebackground='black')
+
+        btn_canvas = self.canvas.create_window(
+            175, 400, anchor="nw", window=play_btn)
+
+        btn_canvas = self.canvas.create_window(
+            275, 400, anchor="nw", window=stop_btn)
+
+    def load_templates(self):
+        cur_dir_path = os.path.abspath(os.path.dirname(__file__))
+        self.templates.append(
+            cv2.imread(os.path.join(cur_dir_path, 'template3.png'), 0))
+        self.templates.append(
+            cv2.imread(os.path.join(cur_dir_path, 'template4.png'), 0))
+        self.templates.append(
+            cv2.imread(os.path.join(cur_dir_path, 'template5.png'), 0))
+        self.templates.append(
+            cv2.imread(os.path.join(cur_dir_path, 'template6.png'), 0))
+
+    def start(self):
+        im1 = pyautogui.screenshot()
+        im1 = np.asarray(im1.convert(mode='L'))
+        for template in self.templates:
+            res = cv2.matchTemplate(im1, template, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(res >= self.threshold)
+            if loc[0].size != 0:
+                pyautogui.click(list(zip(*loc[::-1]))[0])
+        self.task = self.window.after(3000, self.start)
+
+    def stop(self):
+        if self.task is not None:
+            self.window.after_cancel(self.task)
+        else:
+            print("Oops, Something Went wrong")
+
+    def run(self):
+        self.window.mainloop()
 
 
-def start():
-    im1 = pyautogui.screenshot()
-    im1 = np.asarray(im1.convert(mode='L'))
-    res = cv2.matchTemplate(im1, template3, cv2.TM_CCOEFF_NORMED)
-    loc = np.where(res >= threshold)
-    if(loc[0].size != 0):
-        pyautogui.click(list(zip(*loc[::-1]))[0])
-    res = cv2.matchTemplate(im1, template4, cv2.TM_CCOEFF_NORMED)
-    loc = np.where(res >= threshold)
-    if(loc[0].size != 0):
-        pyautogui.click(list(zip(*loc[::-1]))[0])
-    res = cv2.matchTemplate(im1, template5, cv2.TM_CCOEFF_NORMED)
-    loc = np.where(res >= threshold)
-    if(loc[0].size != 0):
-        pyautogui.click(list(zip(*loc[::-1]))[0])
-    res = cv2.matchTemplate(im1, template6, cv2.TM_CCOEFF_NORMED)
-    loc = np.where(res >= threshold)
-    if(loc[0].size != 0):
-        pyautogui.click(list(zip(*loc[::-1]))[0])
-    temp = window.after(3000, start)
-    if(task != []):
-        task.pop()
-    task.append(temp)
-    print(task)
-
-
-def stop():
-    if(task != []):
-        window.after_cancel(task[0])
-    else:
-        print("Oops,Something Went wrong")
-
-
-canvas = Canvas(window, width=500, height=500)
-canvas.pack(fill='both', expand=True)
-canvas.config(bg='black')
-Font_tuple = ("Comic Sans MS", 20, "bold")
-
-play_btn = tkfontawesome.icon_to_image(
-    name="play", fill='white', scale_to_width=40)
-stop_btn = tkfontawesome.icon_to_image(
-    name='stop', fill='white', scale_to_width=40)
-
-img = PhotoImage(file=cur_dir_path+'\\YoutubeLogo.png')
-canvas.create_image(140, 100, image=img, anchor="nw")
-
-canvas.create_text(240, 50, text='Youtube Ad Skipper',
-                   font=Font_tuple, fill='white')
-
-b1 = Button(window,
-            command=start, width=60, height=60, image=play_btn, bg='black', borderwidth=0)
-
-b2 = Button(window,
-            command=stop, width=60, height=60, image=stop_btn, bg='black', borderwidth=0)
-
-btn_canvas = canvas.create_window(175, 400, anchor="nw", window=b1)
-
-btn_canvas = canvas.create_window(275, 400, anchor="nw", window=b2)
-
-window.mainloop()
+if __name__ == '__main__':
+    app = YoutubeAdSkipper()
+    app.run()
